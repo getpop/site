121dea105d2f5bf48abe6923b406f9f55ad241c3
Leo <losoviz@gmail.com>
2019-10-15 16:15:57 +0800

    Added dependency on API

---
+++ src/ModuleProcessors/AbstractModuleProcessor.php
@@ -0,0 +1,8 @@
+<?php
+namespace PoP\SiteBuilder\ModuleProcessors;
+use PoP\API\ModuleProcessors\ModuleProcessorTrait;
+
+abstract class AbstractModuleProcessor extends \PoP\ConfigurationComponentModel\ModuleProcessors\AbstractModuleProcessor
+{
+    use ModuleProcessorTrait;
+}
