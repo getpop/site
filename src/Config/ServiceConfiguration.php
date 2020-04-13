a22a2f8b7bbd42fd5064f46d4dceea1d91d79764
Leo <losoviz@gmail.com>
2020-04-13 19:20:25 +0800

    Added declare(strict_types=1);

--- src/Config/ServiceConfiguration.php
+++ src/Config/ServiceConfiguration.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace PoP\Site\Config;
 
 use PoP\Resources\DefinitionGroups;
--- src/Engine/Engine.php
+++ src/Engine/Engine.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace PoP\Site\Engine;
 
 class Engine extends \PoP\Application\Engine\Engine
