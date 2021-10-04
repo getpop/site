2569399fadd06c20266eece3485237a3f909006d
leoloso <leo@getpop.org>
2021-10-04 09:14:38 +0000

    Merge pull request #1116 from leoloso/make-autowire-functions-final
    Made all autowire functions final

--- src/Engine/Engine.php
+++ src/Engine/Engine.php
@@ -4,16 +4,16 @@ declare(strict_types=1);
 
 namespace PoP\Site\Engine;
 
-use Symfony\Contracts\Service\Attribute\Required;
-use PoP\ComponentModel\HelperServices\ApplicationStateHelperServiceInterface;
 use PoP\Application\Engine\Engine as UpstreamEngine;
+use PoP\ComponentModel\HelperServices\ApplicationStateHelperServiceInterface;
+use Symfony\Contracts\Service\Attribute\Required;
 
 class Engine extends UpstreamEngine
 {
     protected ApplicationStateHelperServiceInterface $applicationStateHelperService;
 
     #[Required]
-    public function autowireSiteEngine(
+    final public function autowireSiteEngine(
         ApplicationStateHelperServiceInterface $applicationStateHelperService
     ): void {
         $this->applicationStateHelperService = $applicationStateHelperService;
